bb463f76e89e3e29058cf56347c16186e5b628e2
Your Name <you@example.com>
2024-08-19 22:04:49 +0700

    update

--- src/main/java/com/example/Social_Network/Security/CustomOAuth2UserService/GoogleAuthController.java
+++ src/main/java/com/example/Social_Network/Security/CustomOAuth2UserService/GoogleAuthController.java
@@ -15,10 +15,10 @@ public class GoogleAuthController {
     private final JWTService jwtService;
     private final RestTemplate restTemplate = new RestTemplate();  // Using RestTemplate to call Google API
 
-    private static final String GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token";
-    private static final String CLIENT_ID = "770516629356-oq4r6265tsvftmu6of4br09cbaqan9en.apps.googleusercontent.com";
-    private static final String CLIENT_SECRET = "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";
-    private static final String REDIRECT_URI = "http://localhost:8080/login/oauth2/code/google";
+    private static final String GOOGLE_TOKEN_URL = "";
+    private static final String CLIENT_ID = "";
+    private static final String CLIENT_SECRET = "";
+    private static final String REDIRECT_URI = "";
 
     @GetMapping("/token")
     public String getToken(@RequestParam("code") String code) {
